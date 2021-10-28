f5843be6fc49c5e5ac0e0ec17126b0e4e53973c0
aigash <nicolas.florentino00@gmail.com>
2021-10-28 15:26:06 +0200

    commit pré refonte graphique

--- src/Controller/MatchupsController.php
+++ src/Controller/MatchupsController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\PersonnagesRepository;
 use App\Repository\OtherStuffRepository;
+use ArrayObject;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,9 +15,35 @@ class MatchupsController extends AbstractController
     public function matchups(OtherStuffRepository $otherStuffRepository, $idM, $idE): Response
     {
         $matchups = $otherStuffRepository->findMatchup($idM, $idE);
+        $matchups['runes'] = json_decode('[' . $matchups['runes'] . ']', true);
+        $matchups['items_json'] = json_decode('[' . $matchups['items_json'] . ']', true);
+
+        function record_sort($records, $field, $reverse = false)
+        {
+            $hash = array();
+
+            foreach ($records as $key => $record) {
+                $hash[$record[$field] . $key] = $record;
+            }
+
+            ($reverse) ? krsort($hash) : ksort($hash);
+
+            $records = array();
+
+            foreach ($hash as $record) {
+                $records[] = $record;
+            }
+
+            return $records;
+        }
+
+        $items = record_sort($matchups['items_json'], "type");
+        $runes = record_sort($matchups['runes'], "type");
 
         return $this->render('matchups/matchup.html.twig', [
-            'matchups' => $matchups
+            'matchups' => $matchups,
+            'items' => $items,
+            'runes' => $runes
         ]);
     }
 
